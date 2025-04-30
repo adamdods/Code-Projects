c1f3fee6a0ab407daff47a0b32550e9988791eb6
Adm David Dods <124843558+adamdods@users.noreply.github.com>
2025-04-30 20:31:49 +0200

    Delete COS3711 - Assessment 3 directory

--- COS3711 - Assessment 3/A3Q2/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <QApplication>
-#include "bookinput.h"
-
-int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
-    BookInput bookInput;
-    bookInput.show();
-    return app.exec();
-}
